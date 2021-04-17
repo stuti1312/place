c4d51977ef675cd9d3e10096c7209742b92ca9d8
stuti <stutisinghal1312@gmail.com>
2021-04-17 17:23:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import {GoogleMap, withScriptjs, withGoogleMap} from 'react-google-maps'
+
+function Map() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <GoogleMap
+      defaultZoom={20}
+      default={{lat:29.4662369, lng:77.6961699}}
+    />
+  )
+}
+
+const WrappedMap = withScriptjs(withGoogleMap(Map));
+
+export default function App() {
+  return (
+    <div>
+      <WrappedMap
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places`}
+        loadingElement = { <div style={{height: '100%'}} />}
+        containerElement = { <div style={{height: '100%'}} />}
+        mapElement = { <div style={{height: '100%'}} />}
+      />
     </div>
   );
 }
-
-export default App;
